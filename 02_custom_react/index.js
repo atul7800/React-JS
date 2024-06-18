358b66a00f41df4709bc5691683db9bd2b2a5cd4
atul <atulgupta.d796@gmil.com>
2024-06-19 02:36:00 +0530

    19-6-24

--- 02_custom_react/index.js
+++ 02_custom_react/index.js
@@ -1,9 +1,19 @@
 function createReactElement(reactElement, rootElement){
+    // Version 1
+    /*
     const domElement = document.createElement(reactElement.type);
     domElement.setAttribute('href', reactElement.props.href);
     domElement.setAttribute('target', reactElement.props.target);
     domElement.innerText = reactElement.child;
+    rootElement.appendChild(domElement); 
+    */
 
+    // Version 2
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.child;
+    for(const prop in reactElement.props){
+        domElement.setAttribute(prop, reactElement.props[prop]);
+    }
     rootElement.appendChild(domElement);
 }
 
