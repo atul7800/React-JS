3d53043373a4ce3f93f1b9f53aa08e12b5b8801e
atul <atulgupta.d796@gmil.com>
2024-06-24 06:36:33 +0530

    24-6-24 V1

--- 05_counter_project/src/App.jsx
+++ 05_counter_project/src/App.jsx
@@ -3,20 +3,29 @@ import { useState } from "react"; //useState is a hook to update the ui
 function App() {
 
   //let counter = 5;
-  console.log(typeof useState)
 
   let [counter, setCounter] = useState(0);
 
   const addValue =  () => {
     counter = counter + 1;
-    setCounter(counter);
-    console.log(`value added ${counter}`);
+    
+    if(counter <= 20){
+      setCounter(counter);
+      console.log(`value added ${counter}`);
+    } else{
+      console.log(`Value can't be more than 20`);
+    }
   }
 
   const removeValue = () => {
     counter = counter - 1;
-    setCounter(counter);
-    console.log(`value removed ${counter}`);
+    if(counter<0){
+      setCounter(counter);
+      console.log(`value removed ${counter}`);
+    } else{
+      console.log(`Value can't be negative`);
+    }
+    
   }
 
   return (
