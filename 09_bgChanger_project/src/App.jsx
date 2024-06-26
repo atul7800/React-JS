164bbba2f2944ae3fbb3486b354ba35f5d33de31
atul <atulgupta.d796@gmil.com>
2024-06-26 19:30:25 +0530

    26-6-24 V2

--- 09_bgChanger_project/src/App.jsx
+++ 09_bgChanger_project/src/App.jsx
@@ -1,22 +1,21 @@
 import { useState } from 'react'
-import Box from './components/box.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('white')
 
   return (
-    <>
-      <div className="boxContainer">
-        <Box boxColor='purple' />
-        <Box boxColor='brown' />
-        <Box boxColor='purple' />
-        <Box boxColor="burlywood" />
-        <Box boxColor='yellow' />
-        <Box boxColor='green' />
-        <Box boxColor='gray' />
-      </div>
-    </>
+    <div className='w-full h-screen' style={{backgroundColor:color}}>
+    <div className='fixed flex justify-center gap-2 rounded-sm align-center bottom-15 p-10 buttonsContainer'>
+      <div onClick={() => setColor('orange')} className='rounded-xl p-5 box' style={{backgroundColor: orange}}>Orange</div>
+      <div onClick={() => setColor('brown')} className='rounded-xl p-5 box' style={{backgroundColor: brown}}>Brown</div>
+      <div onClick={() => setColor('purple')} className='rounded-xl p-5 box' style={{backgroundColor: purple}}>Purple</div>
+      <div onClick={() => setColor('burlywood')} className='rounded-xl p-5 box' style={{backgroundColor: burlywood}}>Burlywood</div>
+      <div onClick={() => setColor('olive')} className='rounded-xl p-5 box' style={{backgroundColor: olive}}>Olive</div>
+      <div onClick={() => setColor('green')} className='rounded-xl p-5 box' style={{backgroundColor: green}}>Green</div>
+      <div onClick={() => setColor('gray')} className='rounded-xl p-5 box' style={{backgroundColor: gray}}>Gray</div>
+    </div>
+</div>
   )
 }
 
