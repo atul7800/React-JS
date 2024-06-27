bf7854f403f129af330f9a196776ab51d52f90be
atul <atulgupta.d796@gmil.com>
2024-06-27 20:10:37 +0530

    27-6-24 V1

--- 09_bgChanger_project/src/App.jsx
+++ 09_bgChanger_project/src/App.jsx
@@ -1,21 +1,20 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [color, setColor] = useState('white')
+  const [color, setColor] = useState('teal')
 
   return (
-    <div className='w-full h-screen' style={{backgroundColor:color}}>
-    <div className='fixed flex justify-center gap-2 rounded-sm align-center bottom-15 p-10 buttonsContainer'>
-      <div onClick={() => setColor('orange')} className='rounded-xl p-5 box' style={{backgroundColor: orange}}>Orange</div>
-      <div onClick={() => setColor('brown')} className='rounded-xl p-5 box' style={{backgroundColor: brown}}>Brown</div>
-      <div onClick={() => setColor('purple')} className='rounded-xl p-5 box' style={{backgroundColor: purple}}>Purple</div>
-      <div onClick={() => setColor('burlywood')} className='rounded-xl p-5 box' style={{backgroundColor: burlywood}}>Burlywood</div>
-      <div onClick={() => setColor('olive')} className='rounded-xl p-5 box' style={{backgroundColor: olive}}>Olive</div>
-      <div onClick={() => setColor('green')} className='rounded-xl p-5 box' style={{backgroundColor: green}}>Green</div>
-      <div onClick={() => setColor('gray')} className='rounded-xl p-5 box' style={{backgroundColor: gray}}>Gray</div>
+    <div className='w-full h-screen flex items-end justify-center bg-slate-200' style={{backgroundColor:color}}>
+      <div className='fixed flex justify-center gap-12 rounded-sm align-center bottom-15 p-10 text-white buttonsContainer'>
+        <div onClick={() => setColor('teal')} className='w-36 h-16 rounded-3xl flex items-center justify-center p-5' style={{backgroundColor:"teal", border:"4px solid white"}} >Teal</div>
+        <div onClick={() => setColor('brown')} className='w-36 h-16 rounded-3xl flex items-center justify-center p-5' style={{backgroundColor:"brown", border:"4px solid white"}}>Brown</div>
+        <div onClick={() => setColor('purple')} className='w-36 h-16 rounded-3xl flex items-center justify-center p-5' style={{backgroundColor:"purple", border:"4px solid white"}}>Purple</div>
+        <div onClick={() => setColor('plum')} className='w-36 h-16 rounded-3xl flex items-center justify-center p-5' style={{backgroundColor:"plum", border:"4px solid white"}}>Plum</div>
+        <div onClick={() => setColor('olive')} className='w-36 h-16 rounded-3xl flex items-center justify-center p-5' style={{backgroundColor:"olive", border:"4px solid white"}}>Olive</div>
+        <div onClick={() => setColor('navy')} className='w-36 h-16 rounded-3xl flex items-center justify-center p-5' style={{backgroundColor:"navy", border:"4px solid white"}}>Navy</div>
+        <div onClick={() => setColor('tan')} className='w-36 h-16 rounded-3xl flex items-center justify-center p-5' style={{backgroundColor:"tan", border:"4px solid white"}}>Tan</div>
+      </div>
     </div>
-</div>
   )
 }
 
