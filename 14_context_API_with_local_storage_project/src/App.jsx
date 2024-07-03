bb3521b38dbb86a65e6c9e4ddc31392c3b62ed26
atul <atulgupta.d796@gmil.com>
2024-07-03 07:22:37 +0530

    3-7-24 V1 [Finished Todo]

--- 14_context_API_with_local_storage_project/src/App.jsx
+++ 14_context_API_with_local_storage_project/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { TodoProvider } from "./contexts";
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -40,8 +42,8 @@ function App() {
   }, []);
 
   useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos))
-  }, [todos])
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <TodoProvider
@@ -52,9 +54,15 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4">
+            <TodoForm />
+          </div>
           <div className="flex flex-wrap gap-y-3">
-            {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
--- 14_context_API_with_local_storage_project/src/components/TodoForm.jsx
+++ 14_context_API_with_local_storage_project/src/components/TodoForm.jsx
@@ -27,6 +27,7 @@ function TodoForm() {
         type="text"
         placeholder="Write Todo..."
         className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        onChange={(e) => setTodo(e.target.value)}
       />
       <button
         type="submit"
