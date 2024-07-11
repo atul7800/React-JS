5f6661a1958e716412287a16412a47790bd3d724
atul <atulgupta.d796@gmil.com>
2024-07-11 19:19:11 +0530

    11-7-24 V4

--- by_ProCodrr/33_Forms/src/components/ExpenseForm.jsx
+++ by_ProCodrr/33_Forms/src/components/ExpenseForm.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 
 function ExpenseForm() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    console.log("Before for");
+    for (const value of formData.values()) {
+      console.log(value);
+    }
+  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const expense = { ...getFormData(e.target), id: crypto.randomUUID() };
+  //   setExpenses((prevState) => [...prevState, expense]);
+  //   e.target.reset();
+  // };
+
   return (
-    <form className="expense-form">
+    <form onSubmit={handleSubmit} className="expense-form">
       <div className="input-container">
         <label htmlFor="title">Title</label>
         <input id="title" />
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
-        <input id="category" />
+        <select id="category">
+          <option hidden>Select category</option>
+          <option value="grocery">Grocery</option>
+          <option value="clothes">Clothes</option>
+          <option value="bills">Bills</option>
+          <option value="education">Education</option>
+          <option value="medicine">Medicine</option>
+        </select>
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
