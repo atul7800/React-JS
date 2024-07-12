269d60c0af2968843bdcc19744882b38c13a5336
atul <atulgupta.d796@gmil.com>
2024-07-12 15:36:15 +0530

    12-7-24 V2

--- by_ProCodrr/33_Forms/src/components/ExpenseForm.jsx
+++ by_ProCodrr/33_Forms/src/components/ExpenseForm.jsx
@@ -1,33 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ExpenseForm({ setExpenses }) {
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [amount, setAmount] = useState("");
+  // console.log(`Title : ${title} | Category : ${category} | Amount : ${amount}`);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    setExpenses((prevValue) => [
-      ...prevValue,
-      { ...getFormData(e.target), id: crypto.randomUUID() },
-    ]);
+
+    //chek the validity of the form manually [not working]
+    // if (!e.target.checkValidity()) {
+    //   console.log("Inside if");
+    //   e.target.reportValidity();
+    //   return;
+    // }
+    //
+
+    const expenses = { title, category, amount, id: crypto.randomUUID() };
+    console.log(expenses);
+    setExpenses((prevValue) => [...prevValue, expenses]);
+
+    e.target.reset();
   };
 
-  const getFormData = (form) => {
-    const formData = new FormData(form);
-    const data = {};
-    for (const [key, value] of formData.entries()) {
-      data[key] = value;
-    }
-    return data;
-  };
+  // const getFormData = (form) => {
+  //   const formData = new FormData(form);
+  //   const data = {};
+  //   for (const [key, value] of formData.entries()) {
+  //     data[key] = value;
+  //   }
+  //   return data;
+  // };
 
   return (
     <form onSubmit={handleSubmit} className="expense-form">
       <div className="input-container">
         <label htmlFor="title">Title</label>
-        <input id="title" name="title" />
+        <input
+          required
+          id="title"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
-        <select id="category" name="category">
-          <option hidden>Select category</option>
+        <select
+          required
+          id="category"
+          name="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="" hidden>
+            Select category
+          </option>
           <option value="grocery">Grocery</option>
           <option value="clothes">Clothes</option>
           <option value="bills">Bills</option>
@@ -37,7 +66,14 @@ function ExpenseForm({ setExpenses }) {
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
-        <input id="amount" name="amount" />
+        <input
+          required
+          id="amount"
+          name="amount"
+          type="number"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
       </div>
       <button className="add-btn">Add</button>
     </form>
