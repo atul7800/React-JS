00739198841b0839d655d293cc552e383b0d1bc7
atul <atulgupta.d796@gmil.com>
2024-07-19 00:55:06 +0530

    19-7-24 V1

--- by_ProCodrr/33_Forms/src/components/ExpenceTable copy.jsx
+++ by_ProCodrr/33_Forms/src/components/ExpenceTable copy.jsx
@@ -1,43 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { FaArrowUp, FaArrowDown } from "react-icons/fa6";
 
 function ExpenceTable({ expenses }) {
   //const [totalAmount, setTotalAmount] = useState(0);
   let total = 0;
   const [filteredData, setFilteredData] = useState(expenses);
   const [value, setValue] = useState("All");
-  const [lowToHigh, setLowToHigh] = useState(true);
-  const [filterQuery, setFilterQuery] = useState(true);
-
-  useEffect(() => {
-    sort();
-  }, []);
 
   //update total amount
   filteredData.map((expense) => {
     total += expense.amount;
   });
 
-  const filterData = (e) => {
+  const handelOnChange = (e) => {
     setValue(e.target.value);
     if (e.target.value === "All") {
+      console.log("inside if");
       return setFilteredData(expenses);
     } else {
+      console.log("inside else");
       setFilteredData(
         expenses.filter(
           (expense) => expense.category === e.target.value.toLowerCase()
         )
       );
-    }
-  };
-
-  const sort = () => {
-    if (lowToHigh) {
-      filteredData.sort((a, b) => a.amount - b.amount);
-      setLowToHigh(!lowToHigh);
-    } else {
-      filteredData.sort((a, b) => b.amount - a.amount);
-      setLowToHigh(!lowToHigh);
     }
   };
 
@@ -47,7 +32,7 @@ function ExpenceTable({ expenses }) {
         <tr>
           <th>Title</th>
           <th>
-            <select value={value} onChange={filterData}>
+            <select value={value} onChange={handelOnChange}>
               <option value="All">All</option>
               <option value="Groccery">Groccery</option>
               <option value="Clothes">Clothes</option>
@@ -59,12 +44,24 @@ function ExpenceTable({ expenses }) {
           <th className="amount-column">
             <div>
               <span>Amount</span>
-
-              {lowToHigh ? (
-                <FaArrowUp onClick={sort} />
-              ) : (
-                <FaArrowDown onClick={sort} />
-              )}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="10"
+                viewBox="0 0 384 512"
+                className="arrow up-arrow"
+              >
+                <title>Ascending</title>
+                <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="10"
+                viewBox="0 0 384 512"
+                className="arrow down-arrow"
+              >
+                <title>Descending</title>
+                <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+              </svg>
             </div>
           </th>
         </tr>
